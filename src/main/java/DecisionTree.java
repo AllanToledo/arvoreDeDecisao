5d61074456fb834f8b6bfde4d608a41004e382ca
AllanToledo <a.llantoledo@hotmail.com>
2023-10-09 11:56:01 -0400

    Initial commit

---
+++ src/main/java/DecisionTree.java
@@ -0,0 +1,141 @@
+import java.util.*;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.log;
+
+public class DecisionTree {
+    public static class DecisionNode {
+        public Integer resultado;
+        public DecisionNode isTrue;
+        public DecisionNode isFalse;
+        public Integer threshold;
+        public String key;
+        public Integer deepth = 0;
+
+        public Integer eval(Map<String, Integer> example) {
+            if (resultado != null) return resultado;
+            if (example.get(this.key) >= threshold)
+                return isTrue.eval(example);
+            return isFalse.eval(example);
+        }
+
+    }
+
+    DecisionNode rootNode;
+
+    public double entropy(List<Map<String, Integer>> data, String _class) {
+        if (data.size() == 0) return 0;
+        Map<Integer, Double> fractions = new HashMap<>();
+        Double fraction = 1.0 / data.size();
+        for (var example : data) {
+            Integer value = example.get(_class);
+            fractions.put(value, fractions.getOrDefault(value, 0.0) + fraction);
+        }
+        return -fractions.values().stream().reduce(0.0, (r, pi) -> r + pi * (log(pi) / log(2)));
+    }
+
+    public double gainClass(List<Map<String, Integer>> data, String s, String a) {
+        if(s.equals(a)) return 0;
+        Map<Integer, Integer> values = new HashMap<>();
+        for (var row : data) {
+            values.put(row.get(a), values.getOrDefault(row.get(a), 0) + 1);
+        }
+        double entropyS = entropy(data, s);
+        double gain = entropyS;
+        for (var value : values.entrySet()) {
+            double sizeGreater = values.entrySet().stream().map(e -> (e.getKey() >= value.getKey() ? e.getValue() : 0))
+                    .reduce(0, Integer::sum);
+            double sizeLess = values.entrySet().stream().map(e -> (e.getKey() < value.getKey() ? e.getValue() : 0))
+                    .reduce(0, Integer::sum);
+
+            var valuesGreater = data.stream().filter((row) -> row.get(a) >= value.getKey()).toList();
+            var valuesLess = data.stream().filter((row) -> row.get(a) < value.getKey()).toList();
+            var gainCalculated = sizeGreater / data.size() * entropy(valuesGreater, s) + sizeLess / data.size() * entropy(valuesLess, s);
+            gain = Math.min(gain, gainCalculated);
+        }
+
+        return entropyS - gain;
+    }
+
+    public Map<Integer, Double> gainAtribute(List<Map<String, Integer>> data, String s, String a) {
+        Map<Integer, Integer> values = new HashMap<>();
+        for (var row : data) {
+            values.put(row.get(a), values.getOrDefault(row.get(a), 0) + 1);
+        }
+        double entropyS = entropy(data, s);
+        Map<Integer, Double> gain = new HashMap<>();
+        for (var value : values.entrySet()) {
+            double sizeGreater = values.entrySet().stream().map(e -> (e.getKey() >= value.getKey() ? e.getValue() : 0))
+                    .reduce(0, Integer::sum);
+            double sizeLess = values.entrySet().stream().map(e -> (e.getKey() < value.getKey() ? e.getValue() : 0))
+                    .reduce(0, Integer::sum);
+
+            var valuesGreater = data.stream().filter((row) -> row.get(a) >= value.getKey()).toList();
+            var valuesLess = data.stream().filter((row) -> row.get(a) < value.getKey()).toList();
+
+            gain.put(value.getKey(), entropyS - (sizeGreater / data.size() * entropy(valuesGreater, s) + sizeLess / data.size() * entropy(valuesLess, s)));
+        }
+        return gain;
+    }
+
+    public void fit(List<Map<String, Integer>> data, String s) {
+        rootNode = new DecisionNode();
+        fit(data, s, rootNode);
+    }
+
+    private void fit(List<Map<String, Integer>> data, String s, DecisionNode node) {
+        double entropyS = entropy(data, s);
+        if (abs(entropyS) <= 0.01 || node.deepth > 5) {
+            node.resultado = data.get(0).get(s);
+            return;
+        }
+
+        AbstractMap.SimpleEntry<String, Double> maxGain = new AbstractMap.SimpleEntry<>("", -1.0);
+
+        for (var column : data.get(0).keySet()) {
+            double gain = gainClass(data, s, column);
+            if (maxGain.getValue() < gain) {
+                maxGain = new AbstractMap.SimpleEntry<>(column, gain);
+            }
+        }
+
+        Map.Entry<Integer, Double> selectedAtribute = gainAtribute(data, s, maxGain.getKey())
+                .entrySet().stream().max((a, b) -> (int) (a.getValue() - b.getValue())).orElseThrow();
+
+        node.threshold = selectedAtribute.getKey();
+        node.key = maxGain.getKey();
+        node.isTrue = new DecisionNode();
+        node.isFalse = new DecisionNode();
+        node.isTrue.deepth = node.deepth + 1;
+        node.isFalse.deepth = node.deepth + 1;
+
+        fit(data.stream().filter(row -> row.get(node.key) >= node.threshold).toList(), s, node.isTrue);
+        fit(data.stream().filter(row -> row.get(node.key) < node.threshold).toList(), s, node.isFalse);
+
+    }
+
+    public Integer eval(Map<String, Integer> example) {
+        if (rootNode == null) return null;
+        return rootNode.eval(example);
+    }
+
+    @Override
+    public String toString(){
+        return toString(rootNode, "");
+    }
+
+    String toString(DecisionNode node, String identation) {
+        StringBuilder sb = new StringBuilder();
+        if(node.resultado != null) {
+            sb.append(identation).append("classe ").append(node.resultado);
+            return sb.toString();
+        }
+        String nextIdentation = identation + "    ";
+        sb.append(identation).append(node.key).append(" >= ").append(node.threshold).append(": \n");
+        sb.append(toString(node.isTrue, nextIdentation)).append("\n");
+        sb.append(identation).append(node.key).append(" < ").append(node.threshold).append(": \n");
+        sb.append(toString(node.isFalse, nextIdentation)).append("\n");
+        return sb.toString();
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    public enum Personagens {
+        HOMER, BART, LISA
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        List<Map<String, Integer>> data = loadData("src\\main\\resources\\personagens.csv");
+        Map<String, Integer> example = new HashMap<>();
+        example.put("idade", 13);
+        example.put("altura", 158);
+        example.put("peso", 40);
+        example.put("sexo", 0);
+
+        DecisionTree decisionTree = new DecisionTree();
+        decisionTree.fit(data, "personagem");
+
+        Personagens personagem = Personagens.values()[decisionTree.eval(example)];
+        System.out.println(personagem);
+        System.out.println(decisionTree);
+    }
+
+    private static List<Map<String, Integer>> loadData(String path) throws FileNotFoundException {
+        List<Map<String, Integer>> data = new ArrayList<>();
+        Scanner sc = new Scanner(new File(path));
+        List<String> labels = new ArrayList<>(List.of(sc.nextLine().split(",")));
+        while (sc.hasNext()) {
+            String[] values = sc.nextLine().split(",");
+            Map<String, Integer> row = new HashMap<>();
+            for (int i = 0; i < labels.size(); i++)
+                row.put(labels.get(i), Integer.parseInt(values[i]));
+            data.add(row);
+        }
+        sc.close();
+        return data;
+    }
+}
